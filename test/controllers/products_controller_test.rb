8eeb3922187dd57993a5b87782f2fbe52cdf5473
Santiago dos Santos <santiiagodossantos@icloud.com>
2023-07-08 13:07:56 -0300

    Validaciones

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,41 +1,50 @@
-require 'test_helper'
+require "test_helper"
 
 class ProductsControllersTest < ActionDispatch::IntegrationTest
+  test "render a list of products" do
+    get products_path
 
-    test 'render a list of products' do
-        get products_path
-        
-        assert_response :success
-        assert_select '.product', 2   # .product le pega a la clase html
+    assert_response :success
+    assert_select ".product", 2   # .product le pega a la clase html
+  end
 
-    end
+  test "render a detailed product page" do
+    get product_path(products(:ps3))  # Busca un product del fixture
 
-    test 'render a detailed product page' do
-        get product_path(products(:ps3))  # Busca un product del fixture
-    
-        assert_response :success
-        assert_select '.title', 'PS3'    # Comparamos con el titulo renderizado del html
-        assert_select '.description', 'Vieja'
-        assert_select '.price', '200'
-    end
+    assert_response :success
+    assert_select ".title", "PS3"    # Comparamos con el titulo renderizado del html
+    assert_select ".description", "Vieja"
+    assert_select ".price", "200"
+  end
 
-    test 'render a new product form' do
-        get new_product_path
+  test "render a new product form" do
+    get new_product_path
 
-        assert_response :success
-        assert_select 'form'           # Cheuqeamos que haya una etiqueta form
-    end
+    assert_response :success
+    assert_select "form"           # Cheuqeamos que haya una etiqueta form
+  end
 
-    test 'allow to create a new product' do
-        post products_path, params: {
-            product: {
-                title: 'Nintendo 64',
-                description: '',
-                price: 45
-            }
-        }
+  test "allow to create a new product" do
+    post products_path, params: {
+                          product: {
+                            title: "Nintendo 64",
+                            description: "Una descripcion",
+                            price: 45,
+                          },
+                        }
 
-        assert_redirected_to products_path
-    end
+    assert_redirected_to products_path
+  end
 
+  test "does not allow to create a new product with empty fields" do
+    post products_path, params: {
+                          product: {
+                            title: "",
+                            description: "Una descripcion",
+                            price: 45,
+                          },
+                        }
+
+    assert_response :unprocessable_entity
+  end
 end
